6cef0b2701b445082f77938e9ba3604c49f8e657
Takumi Ueno <zer0@Takumis-MacBook-Pro.local>
2021-11-20 21:44:42 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+var http = require('http').Server(app);
+var socketio = require('socket.io')(http);
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+var io = socketio.listen(4000);
+
+io.on("connection", socket => {
+    console.log("helloe")
+
+    socket.on("number", (data) => {
+        console.log(data.num)
+
+        socket.broadcast.emit('update', {number: data.num})
+    })
+})
+
+
+const PORT = 3000
+
+http.listen(PORT, function () {
+    console.log('server listening. Port:' + PORT);
+});
